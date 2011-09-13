bc31e89fb97da1132898a3129385d90ad0106819
Stas Malolepszy <stas@mozilla.com>
2011-09-13 08:34:15 +0200

    Add a form that doesn't do anything yet

--- apps/upload/views.py
+++ apps/upload/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from upload.forms import UploadFileForm
+
+def index(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # do stuff with request.FILES['file']
+            # redirect to /UUID
+            return HttpResponseRedirect('/')
+    else:
+        form = UploadFileForm()
+    return render_to_response('upload/index.html',
+                              {'form': form},
+                              context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -5,13 +5,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'replaydog.views.home', name='home'),
-    # url(r'^replaydog/', include('replaydog.foo.urls')),
+    url(r'', include('upload.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
 
